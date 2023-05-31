79d74696c5612f5d5bed3e01f87e294f77aadbb5
isneru <diogoclanogueira@gmail.com>
2023-05-31 12:07:13 +0100

    userkey

--- src/server/utils/crypto.ts
+++ src/server/utils/crypto.ts
@@ -3,18 +3,18 @@ import { createCipheriv, createDecipheriv, randomBytes } from "crypto"
 export const keyBuffer = randomBytes(32)
 export const ivBuffer = randomBytes(16)
 
-// text encryption
+function getKeyBuffer(userKey?: string) {
+	if (userKey) {
+		const keyBufferFill = Buffer.alloc(32, 0)
+		const userKeyBuffer = Buffer.from(userKey, "utf-8")
+		return Buffer.concat([userKeyBuffer, keyBufferFill]).subarray(0, 32)
+	} else {
+		return keyBuffer
+	}
+}
+
 export function encryptText(text: string, userKey?: string) {
-  const cipher = createCipheriv(
-    "aes-256-cbc",
-    userKey
-      ? Buffer.concat([Buffer.from(userKey, "utf-8"), keyBuffer]).subarray(
-          0,
-          32
-        )
-      : keyBuffer,
-    ivBuffer
-  )
+  const cipher = createCipheriv("aes-256-cbc", getKeyBuffer(userKey), ivBuffer)
   let encrypted = cipher.update(text)
   encrypted = Buffer.concat([encrypted, cipher.final()])
   return encrypted.toString("hex")
@@ -22,33 +22,14 @@ export function encryptText(text: string, userKey?: string) {
 
 export function decryptText(text: string, userKey?: string) {
   const encryptedText = Buffer.from(text, "hex")
-  const decipher = createDecipheriv(
-    "aes-256-cbc",
-    userKey
-      ? Buffer.concat([Buffer.from(userKey, "utf-8"), keyBuffer]).subarray(
-          0,
-          32
-        )
-      : keyBuffer,
-    ivBuffer
-  )
+  const decipher = createDecipheriv("aes-256-cbc", getKeyBuffer(userKey),ivBuffer)
   let decrypted = decipher.update(encryptedText)
   decrypted = Buffer.concat([decrypted, decipher.final()])
   return decrypted.toString()
 }
 
-// image encryption
 export function encryptImage(imageString: string, userKey?: string) {
-  const cipher = createCipheriv(
-    "aes-256-cbc",
-    userKey
-      ? Buffer.concat([Buffer.from(userKey, "utf-8"), keyBuffer]).subarray(
-          0,
-          32
-        )
-      : keyBuffer,
-    ivBuffer
-  )
+  const cipher = createCipheriv("aes-256-cbc", getKeyBuffer(userKey), ivBuffer)
   let encrypted = cipher.update(imageString)
   encrypted = Buffer.concat([encrypted, cipher.final()])
   return encrypted.toString("hex")
@@ -56,32 +37,14 @@ export function encryptImage(imageString: string, userKey?: string) {
 
 export function decryptImage(image: string, userKey?: string) {
   const encryptedText = Buffer.from(image, "hex")
-  const decipher = createDecipheriv(
-    "aes-256-cbc",
-    userKey
-      ? Buffer.concat([Buffer.from(userKey, "utf-8"), keyBuffer]).subarray(
-          0,
-          32
-        )
-      : keyBuffer,
-    ivBuffer
-  )
+  const decipher = createDecipheriv("aes-256-cbc", getKeyBuffer(userKey),ivBuffer)
   let decrypted = decipher.update(encryptedText)
   decrypted = Buffer.concat([decrypted, decipher.final()])
   return decrypted.toString()
 }
 
 export function encryptFile(fileString: string, userKey?: string) {
-  const cipher = createCipheriv(
-    "aes-256-cbc",
-    userKey
-      ? Buffer.concat([Buffer.from(userKey, "utf-8"), keyBuffer]).subarray(
-          0,
-          32
-        )
-      : keyBuffer,
-    ivBuffer
-  )
+  const cipher = createCipheriv("aes-256-cbc", getKeyBuffer(userKey), ivBuffer)
   let encrypted = cipher.update(fileString)
   encrypted = Buffer.concat([encrypted, cipher.final()])
   return encrypted.toString("hex")
@@ -89,16 +52,7 @@ export function encryptFile(fileString: string, userKey?: string) {
 
 export function decryptFile(file: string, userKey?: string) {
   const encryptedText = Buffer.from(file, "hex")
-  const decipher = createDecipheriv(
-    "aes-256-cbc",
-    userKey
-      ? Buffer.concat([Buffer.from(userKey, "utf-8"), keyBuffer]).subarray(
-          0,
-          32
-        )
-      : keyBuffer,
-    ivBuffer
-  )
+  const decipher = createDecipheriv("aes-256-cbc", getKeyBuffer(userKey),ivBuffer)
   let decrypted = decipher.update(encryptedText)
   decrypted = Buffer.concat([decrypted, decipher.final()])
   return decrypted.toString()
